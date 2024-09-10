ded61b9be1e96477e40363ac9900aa58fb26af51
ricky <39970880+torickyto@users.noreply.github.com>
2024-09-10 14:07:51 -0600

    grabbing env keys from vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    env: {
+    publicRuntimeConfig: {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,16 @@
-const getEnvironmentVariable = (environmentVariable: string): string => {
-    const unvalidatedEnvironmentVariable = process.env[environmentVariable];
-    if (!unvalidatedEnvironmentVariable) {
-      throw new Error(`Couldn't find environment variable: ${environmentVariable}`);
-    } else {
-      return unvalidatedEnvironmentVariable;
-    }
-  };
-  
-  export const config = {
-    supabaseUrl: getEnvironmentVariable('NEXT_PUBLIC_SUPABASE_URL'),
-    supabaseAnonKey: getEnvironmentVariable('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
-  };
+import getConfig from '../next.config';
+
+const { publicRuntimeConfig } = getConfig();
+
+const getEnvironmentVariable = (key: string): string => {
+  const value = publicRuntimeConfig[key];
+  if (!value) {
+    throw new Error(`Couldn't find environment variable: ${key}`);
+  }
+  return value;
+};
+
+export const config = {
+  supabaseUrl: getEnvironmentVariable('NEXT_PUBLIC_SUPABASE_URL'),
+  supabaseAnonKey: getEnvironmentVariable('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
+};
